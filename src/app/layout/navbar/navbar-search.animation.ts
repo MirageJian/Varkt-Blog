66815cddf3049087e99df19f0229a3f9b73b36c8
zhang <zhang745426@outlook.com>
2019-10-28 01:41:54 +0000

    Finish animation of search box

---
+++ src/app/layout/navbar/navbar-search.animation.ts
@@ -0,0 +1,19 @@
+import {animate, keyframes, state, style, transition, trigger} from "@angular/animations";
+
+export const searchBox = trigger('searchBox', [
+  state('*', style({opacity: 1, left: 0, right: 0})),
+  transition(':enter', [
+    animate('0.33s ease-in', keyframes([
+      style({opacity: 0, left: 'calc(100% - 152px)', right: 40, offset: 0}),
+      style({opacity: 1, left: 'calc(70% - 152px)', right: 28, offset: 0.3}),
+      style({opacity: 1, left: 0, right: 0, offset: 1}),
+    ]))
+  ]),
+  transition(':leave', [
+    animate('0.33s ease-out', keyframes([
+      style({opacity: 1, left: 0, right: 0, offset: 0}),
+      style({opacity: 1, left: 'calc(70% - 152px)', right: 28, offset: 0.7}),
+      style({opacity: 0, left: 'calc(100% - 152px)', right: 40, offset: 1}),
+    ])),
+  ])
+])
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,23 +1,36 @@
-import {Component, Inject, LOCALE_ID, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, Inject, LOCALE_ID, OnDestroy, OnInit} from '@angular/core';
 import {LoginService} from '../../app-services/login.service';
 import {Router, NavigationEnd} from '@angular/router';
 import {Subscription} from 'rxjs';
 import {ResModel} from '../../shared/models';
 import {filter} from 'rxjs/operators';
+import {searchBox} from "./navbar-search.animation";
+import {AppConst} from "../../shared/app-const";
+import {BreakpointObserver, Breakpoints, MediaMatcher} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  animations: [searchBox]
 })
 export class NavbarComponent implements OnInit, OnDestroy {
   subscriptionRouter: Subscription;
+  isShownSearch = false;
 
   constructor(
     private router: Router,
     public loginService: LoginService,
-    @Inject(LOCALE_ID) public localeId
+    @Inject(LOCALE_ID) public localeId,
+    private breakpointObserver: BreakpointObserver
   ) {
+    // if in the desktop width
+    breakpointObserver.observe([Breakpoints.HandsetLandscape, Breakpoints.HandsetPortrait]).subscribe(result => {
+      if (!result.matches) {
+        this.isShownSearch = true;
+      }
+    });
+
   }
 
   ngOnInit() {
@@ -51,6 +64,12 @@ export class NavbarComponent implements OnInit, OnDestroy {
   logout() {
     this.loginService.logout().subscribe(() => this.loginService.userName = null);
     this.router.navigate(['/']).catch();
+  }
+
+  submitSearch() {
+    if (this.breakpointObserver.isMatched([Breakpoints.HandsetLandscape, Breakpoints.HandsetPortrait])) {
+      this.isShownSearch = false;
+    }
   }
 
   ngOnDestroy() {
