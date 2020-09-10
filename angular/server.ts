458b77e1b627f6298300af25828c74c833d338ce
MirageJian <zhang745426@outlook.com>
2020-09-11 00:55:33 +0100

    Name changes. Run npm audit fix with package problems.

--- angular/server.ts
+++ angular/server.ts
@@ -14,7 +14,7 @@ import {createProxyServer} from 'http-proxy';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app() {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/holacode/browser');
+  const distFolder = join(process.cwd(), 'dist/varkt/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
